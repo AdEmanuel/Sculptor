b2631d2610ea72448a6279e75a9c042b64c89532
AdEmanuel <68473645+AdEmanuel@users.noreply.github.com>
2020-11-08 19:07:46 -0300

    Add files via upload

---
+++ Projeto/sculptor.cpp
@@ -0,0 +1,16 @@
+#include "sculptor.h"
+
+
+Sculptor::Sculptor(int _nx, int _ny, int _nz){
+    nx = _nx;
+    ny = _ny;
+    nz = _nz;
+}
+
+ void Sculptor::setColor(float r, float g, float b, float alpha){
+
+ }
+
+ void Sculptor::putVoxel(int x, int y, int z){
+
+ }
