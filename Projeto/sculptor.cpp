50ce1bb2e05fde0dd3411fd25e7cadc0195b0133
AdEmanuel <68473645+AdEmanuel@users.noreply.github.com>
2020-11-11 00:13:49 -0300

    Update sculptor.cpp

--- Projeto/sculptor.cpp
+++ Projeto/sculptor.cpp
@@ -2,10 +2,57 @@
 
 
 Sculptor::Sculptor(int _nx, int _ny, int _nz){
-    // aloca uma matriz voxel com tamanho especificado.
     nx = _nx;
-    ny = _ny; 
+    ny = _ny;
     nz = _nz;
+    
+    // aloca uma matriz voxel com tamanho especificado.
+    v = new Voxel**[nx];
+    if(v==nullptr){
+        cout << "Alocação do v falhou";
+        exit(0);
+    }
+    v[0] = new Voxel*[nx*ny];
+    if(v[0]==nullptr){
+        cout << "Alocação do v[0] falhou";
+        exit(0);
+    }
+    v[0][0] = new Voxel[nx*ny*nz];
+    if(v[0][0]==nullptr){
+        cout << "Alocação do v[0][0] falhou";
+        exit(0);
+    }
+    for (int i=0; i<nx; i++){
+        if(i<(nx-1)){
+            v[i+1] = v[i]+ny;
+        }
+        for(int j=1;j<ny;j++){
+            v[i][j] = v[i][j-1]+nz;
+            if(j==ny-1 && i!=(nx-1)){
+                v[i+1][0] = v[i][j]+nz;
+            }
+        }
+    }
+
+    for(int i = 0; i < nx; i++){
+        for(int j = 0; j < ny; j++){
+            for(int k = 0; k < nz; k++){
+                //avaliar inicializar as cores aqui tambem;
+                v[i][j][k].isOn = false;
+            }
+        }
+    }
+}
+
+Sculptor::~Sculptor(){
+    if(nx==0||ny==0||nz==0){
+        cout << "Destrutor falhou \n";
+        return;
+    }
+    delete [] v[0][0];
+    delete [] v[0];
+    delete [] v;
+    cout << "Destrutor ok";
 }
 
  void Sculptor::setColor(float r, float g, float b, float alpha){
@@ -55,7 +102,6 @@ void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
 }
 
 void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
-    
 //CABE COLOCAR UMA VERIIFCAÇÃO AQUI, EX.: CASO OS PONTOS SEJAM INVÁLIDOS (PONTOS QUE NÃO SATISFAÇAM A EQUAÇÃO DA ESFERA, AVISAR "VALORES INVÁLIDOS, POR FAVOR DIGITE NOVAMENTE")
     
     int i,j,k; // Variáveis contadores
@@ -68,7 +114,7 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
             for(k=0; k<nz; k++){
                 distZ = static_cast<float>(((k-zcenter)/radius));
                 
-               float dist = (distX*distX) + (distY*distY) + (distZ*distZ) // Todas as distâncias somadas.
+               float dist = (distX*distX) + (distY*distY) + (distZ*distZ); // Todas as distâncias somadas.
                    
                    if(dist<=1.00){
                        putVoxel(i,j,k); // Caso a distância seja menor que 1, isso significa que a equação é respeitada e os valores são coerentes, portanto é criado um Voxel no local.
@@ -77,7 +123,7 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
         }
     }
 }
-            
+
 void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
     //Seguindo a mesma lógica do putSphere, porém agora ao invés de chamar putVoxel será chamado a função cutVoxel, para retirar um voxel.
     
@@ -105,8 +151,8 @@ void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
     }
 }
 
-void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int int rx, int ry, int rz){
-    //Para a função do putEllipsoid, a mesma equação da função putSphere pode ser utilizada, mas com a diferença que no cálcul das distâncias o valor dos raios em cada eixo é utilizado, ao invés de um único raio geral.
+void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+//Para a função do putEllipsoid, a mesma equação da função putSphere pode ser utilizada, mas com a diferença que no cálcul das distâncias o valor dos raios em cada eixo é utilizado, ao invés de um único raio geral.
     int i,j,k; // Variáveis contadores
     float distX, distY, distZ; // Variáveis que representam a distânica do centro da esfera para o ponto.
     
@@ -117,7 +163,7 @@ void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int int rx, i
             for(k=0; k<nz; k++){
                 distZ = static_cast<float>(((k-zcenter)/rz));
                 
-               float dist = (distX*distX) + (distY*distY) + (distZ*distZ) // Todas as distâncias somadas.
+               float dist = (distX*distX) + (distY*distY) + (distZ*distZ); // Todas as distâncias somadas.
                    
                    if(dist<=1.00){
                        putVoxel(i,j,k);
@@ -127,27 +173,97 @@ void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int int rx, i
     }
 }
 
-void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int int rx, int ry, int rz){
+void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
     int i,j,k; // Variáveis contadores
-    float distX, distY, distZ; // Variáveis que representam a distânica do centro da esfera para o ponto.
-    
-    for(i=0; i<nx; i++){
-        distX = static_cast<float>(((i-xcenter)/rx));
-        for(j=0; j<ny; j++){
-            distY = static_cast<float>(((j-ycenter)/ry));
-            for(k=0; k<nz; k++){
-                distZ = static_cast<float>(((k-zcenter)/rz));
-                
-               float dist = (distX*distX) + (distY*distY) + (distZ*distZ) // Todas as distâncias somadas.
-                   
-                   if(dist<=1.00){
-                       cutVoxel(i,j,k);
-                       v[i][j][k].r = r;
-                       v[i][j][k].g = g;
-                       v[i][j][k].b = b;
-                       v[i][j][k].a = a;
-                   }
+        float distX, distY, distZ; // Variáveis que representam a distânica do centro da esfera para o ponto.
+        
+        for(i=0; i<nx; i++){
+            distX = static_cast<float>(((i-xcenter)/rx));
+            for(j=0; j<ny; j++){
+                distY = static_cast<float>(((j-ycenter)/ry));
+                for(k=0; k<nz; k++){
+                    distZ = static_cast<float>(((k-zcenter)/rz));
+                    
+                   float dist = (distX*distX) + (distY*distY) + (distZ*distZ); // Todas as distâncias somadas.
+                       
+                       if(dist<=1.00){
+                           cutVoxel(i,j,k);
+                           v[i][j][k].r = r;
+                           v[i][j][k].g = g;
+                           v[i][j][k].b = b;
+                           v[i][j][k].a = a;
+                       }
+                }
+            }
+        }
+}
+
+void Sculptor::writeOFF(char *filename){
+    int NVoxels = 0, NVertices, NFaces, faces = 0;
+
+    ofstream fout;
+    fout.open(filename);
+    if (!fout.is_open()){
+       exit(1);
+       cout << "Não foi possivel gravar no arquivo";
+    }
+    fout << "OFF \n";
+
+    for(int i=0;i<nx;i++){
+        for(int j=0;j<ny;j++){
+            for(int k=0;k<nz;k++){
+                if (v[i][j][k].isOn){
+                    NVoxels++;
+                }
             }
         }
     }
+
+    NVertices = 8*NVoxels;
+    NFaces = 6*NVoxels;
+
+    fout << NVertices << " " << NFaces << " " << 0 << endl;
+    
+    //Vertices do cubo sendo inseridos no aqruivo OFF
+    for(int i=0; i<nx; i++){
+        for(int j=0; j<ny; j++){
+            for(int k=0; k<nz; k++){
+                if(v[i][j][k].isOn){
+                    fout << i-0.5 << " " << j+0.5 << " " << k-0.5 << endl
+                         << i-0.5 << " " << j-0.5 << " " << k-0.5 << endl
+                         << i+0.5 << " " << j-0.5 << " " << k-0.5 << endl
+                         << i+0.5 << " " << j+0.5 << " " << k-0.5 << endl
+                         << i-0.5 << " " << j+0.5 << " " << k+0.5 << endl
+                         << i-0.5 << " " << j-0.5 << " " << k+0.5 << endl
+                         << i+0.5 << " " << j-0.5 << " " << k+0.5 << endl
+                         << i+0.5 << " " << j+0.5 << " " << k+0.5 << endl;
+                 }
+            }
+        }
+    }
+    //Faces do cubo sendo inseridos no arquivo OFF
+    for(int i=0; i<nx; i++){
+        for(int j=0; j<ny; j++){
+            for(int k=0; k<nz; k++){
+                if(v[i][j][k].isOn){
+                    fout << 4 << " " << 0+faces << " " << 3+faces << " " << 2+faces << " " << v[i][j][k].r << " "
+                         << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << endl
+                         << 4 << " " << 4+faces << " " << 5+faces << " " << 6+faces << " " << v[i][j][k].r << " "
+                         << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << endl
+                         << 4 << " " << 0+faces << " " << 1+faces << " " << 5+faces << " " << v[i][j][k].r << " "
+                         << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << endl
+                         << 4 << " " << 0+faces << " " << 4+faces << " " << 7+faces << " " << v[i][j][k].r << " "
+                         << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << endl
+                         << 4 << " " << 3+faces << " " << 7+faces << " " << 6+faces << " " << v[i][j][k].r << " "
+                         << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << endl
+                         << 4 << " " << 1+faces << " " << 2+faces << " " << 6+faces << " " << v[i][j][k].r << " "
+                         << v[i][j][k].g << " " << v[i][j][k].b << " " << v[i][j][k].a << endl;
+                    faces = faces+8;
+
+                 }
+            }
+        }
+    }
+
+    fout.close();
 }
