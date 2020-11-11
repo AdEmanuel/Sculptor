dcc657b1eb89222c91280ac375c026ab39b53916
AdEmanuel <68473645+AdEmanuel@users.noreply.github.com>
2020-11-11 00:14:47 -0300

    Update sculptor.cpp

--- Projeto/sculptor.cpp
+++ Projeto/sculptor.cpp
@@ -98,7 +98,6 @@ void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
             }
         }
     }
-
 }
 
 void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
